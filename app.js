1b06c19c4d8db23d6a18485aba34283669d63938
AC1802 <jacantos.jac@gmail.com>
2023-06-02 16:55:38 +0200

    Création des dossiers/fichiers pour l'utilisatieur

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://alexiecantos:hWnqps8cD6q3e1Br@cluster0.ze0mbo8.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use('/api/')
+
+module.exports = app;
