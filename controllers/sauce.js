412a105c47428bced1237249738546fece324e4c
AC1802 <jacantos.jac@gmail.com>
2023-06-27 13:43:57 +0200

    Mis à jour du controller pour les sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,3 +1,4 @@
+const { userInfo } = require('os');
 const Sauce = require('../models/sauce');
 const fs = require('fs');
 
@@ -17,7 +18,7 @@ exports.createSauce = (req, res, next) => {
 
     sauce.save()
     .then(() => { res.status(201).json({message: 'Sauce enregistrée !'})})
-    .catch(error => { res.status(400).json( { error })})
+    .catch(error => { res.status(400).json( { error })});
 };
 
 
@@ -60,10 +61,35 @@ exports.modifySauce = (req, res, next) => {
 };
 
 exports.likeSauce = (req, res, next) => {
-    // Sauce.findOne({ _id: req.params.id })
-    //     .then(sauce => {
-
-    //     })
+    if (req.body.like == 1) {
+        Sauce.updateOne({_id: req.params.id}, {$inc: {likes: +1}, $push: {usersLiked: req.body.userId}})
+            .then(() => res.status(200).json({message: 'Sauce likée'}))
+            .catch(error => {res.status(400).json({ error })});
+    };
+    
+    if (req.body.like == -1) {
+        Sauce.updateOne({_id: req.params.id}, {$inc: {dislikes: +1}, $push: {usersDisliked: req.body.userId}})
+            .then(() => res.status(200).json({message: 'Sauce dislikée'}))
+            .catch(error => {res.status(400).json({ error })})
+    }; 
+        
+    if (req.body.like == 0) {
+        Sauce.findOne({_id: req.params.id})
+            .then((sauce) => {
+                if (sauce.usersLiked.includes(req.body.userId)) {
+                    Sauce.updateOne({_id: req.params.id}, {$inc: {likes: -1}, $pull: {usersLiked: req.body.userId}})
+                        .then(() => res.status(200).json({ message: 'Like retiré'}))
+                        .catch((error) => res.status(400).json({error}));
+                } else if (sauce.usersDisliked.includes(req.body.userId)) {
+                    Sauce.updateOne({_id: req.params.id}, {$inc: {dislikes: -1}, $pull: {usersDisliked: req.body.userId}})
+                        .then(() => res.status(200).json({ message: 'Dislike retiré'}))
+                        .catch((error) => res.status(400).json({error}));
+                }
+            })
+            .catch((error) => res.status(404).json({ error }));
+    };
+    
+        
 };
 
 exports.deleteSauce = (req, res, next) => {
